fb376fd16a3b9902ec871df9b55767cc75e008db
Jonathan Liu <liu.z.jonathan@gmail.com>
2018-12-03 16:16:46 -0800

    Join Room #1

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import JoinScreen from '../screens/JoinScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+const JoinStack = createStackNavigator({
+  Join: JoinScreen,
+});
+
+JoinStack.navigationOptions = {
+  tabBarLabel: 'Join',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+    />
+  ),
+};
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen,
+});
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  JoinStack,
+  SettingsStack,
+});
---
+++ screens/JoinScreen.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+} from 'react-native';
+import { TextField } from 'react-native-material-textfield';
+
+class JoinScreen extends Component {
+  static navigationOptions = {
+    title: 'Join',
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      roomCode: '',
+      placeholder: 'Enter room code',
+    }
+  }
+
+  onSubmit = () => {
+    
+  };
+
+  render() {
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+        <Text style={{textAlign: 'center', fontSize: 25}}>Join a room:</Text>
+        <TextInput 
+          style={styles.textInput}
+          autoCapitalize="none"
+          placeholder={this.state.placeholder}
+          maxLength={6}
+          returnKeyType="go"
+          onChangeText={roomCode => this.setState({ roomCode })}
+          onFocus={() => this.setState({ placeholder: '' })}
+          onBlur={() => this.setState({ placeholder: 'Enter room code' })}
+          onSubmitEditing={this.onSubmit}
+        />
+        <Button
+          title="Join"
+          onPress={this.onSubmit}
+        />
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    paddingLeft: 20,
+    paddingRight: 20,
+    backgroundColor: '#fff',
+    paddingBottom: 200,
+  },
+  textInput: {
+    height: 50,
+    fontSize: 40,
+    padding: 5,
+    textAlign: 'center',
+    borderBottomColor: '#000',
+    borderBottomWidth: 1,
+    marginBottom: 20,
+  },
+});
+
+export default JoinScreen;
